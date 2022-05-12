63b183d0a8d1317ca3d44e19fb2994ebf08cd6c0
Valerii Bihun <valeriibihun.co@gmail.com>
2022-05-11 23:12:08 -0600

    Adding openAI

--- src/Components/Response/Response.jsx
+++ src/Components/Response/Response.jsx
@@ -2,6 +2,28 @@ import React, { useState } from "react";
 
 const Response = (props) => {
     let userPrompt = props.prompt
+
+    const data = {
+
+    prompt: "Write a poem about a dog wearing skis",
+    temperature: 0.5,
+    max_tokens: 64,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+   };
+    
+   fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer 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`,
+    },
+    body: JSON.stringify(data),
+   });
+   
+
+
   return (
       <div>
 <div>User said: {userPrompt}</div>
